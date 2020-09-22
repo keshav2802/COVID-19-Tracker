7f283853ac09d5800998a6523151b7098c9445bb
keshavism28 <keshavism28@gmail.com>
2020-09-22 19:24:08 +0530

    fetched daily data from API and created the chart

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
+
+// function to fetch total data
 const fetchData = async () => {
   try {
     const {data} = await axios.get(url);
@@ -18,6 +20,16 @@ const fetchData = async () => {
   }
 }
 
-export {fetchData};
+// function to fetch daily data
+const fetchDailyData = async () => {
+  try {
+    const {data} = await axios.get(`${url}/daily`);
+    return data;
+  } catch (error) {
+    
+  }
+}
+
+export {fetchData, fetchDailyData};
 
 // The fetchData is an asynchronous function. We are gonna use the most modern way to make it asynchronous.i.e, the async-await method. It deals with promises in the same way that the fetch('url').then(data, callback).catch(err, callback) method deals with, but async-await method is much easier to read and write. We use a try-catch block inside of this method.
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from '../../api/index';
+import {Line, Bar} from 'react-chartjs-2';
+import './Chart.css';
 
 const Chart = () => {
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setDailyData(await fetchDailyData());
+    }
+    
+    fetchData();
+  });
+
+  // We are gonna have two different charts.One a bar chart for each specific country and another a line chart for the global daily cases.
+  
+  const lineChart = (
+    dailyData.length !== 0 ? 
+    <Line 
+      data={{
+        labels: dailyData.map(x => x.reportDate),
+        datasets: [{
+          data: dailyData.map(x => x.confirmed.total),
+          label: 'Infected',
+          borderColor: '#3333ff',
+          fill: true
+        },{
+          data: dailyData.map(x => x.deaths.total),
+          label: 'Deaths',
+          borderColor: 'red',
+          backgroundColor: 'rgba(255, 0, 0, 0.5)',
+          fill: true
+        }]
+      }}
+    /> : null
+  )
+
   return (
-    <h1>Chart</h1>
+    <div className="Chart">
+      {lineChart}
+    </div>
   )
 }
 
