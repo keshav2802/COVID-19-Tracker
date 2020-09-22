9e08590652f324bf24343e318c415c44e35e5224
keshavism28 <keshavism28@gmail.com>
2020-09-22 13:29:14 +0530

    created the required components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     )
   }
