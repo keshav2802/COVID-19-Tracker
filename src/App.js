0d559c52b10ce722d9ed3a69951e15541304458f
keshavism28 <keshavism28@gmail.com>
2020-09-22 13:38:30 +0530

    added some initial styles to the App component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, {Component} from 'react';
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Cards />
         <CountryPicker />
         <Chart />
